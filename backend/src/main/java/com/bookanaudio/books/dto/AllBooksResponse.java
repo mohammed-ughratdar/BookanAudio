1dd20ffb571649ed921dcdef802e66dcfcfd16df
mohmaaya <mohammed.ughratdar@fau.de>
2024-10-27 19:29:28 +0100

    added book Get endpoint

---
+++ backend/src/main/java/com/bookanaudio/books/dto/AllBooksResponse.java
@@ -0,0 +1,49 @@
+package com.bookanaudio.books.dto;
+
+public class AllBooksResponse {
+    private Long id;
+    private String name;
+    private String author;
+    private String genre;
+    private String chapterNamingScheme;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getChapterNamingScheme() {
+        return this.chapterNamingScheme;
+    }
+
+    public void setChapterNamingScheme(String chapterNamingScheme) {
+        this.chapterNamingScheme = chapterNamingScheme;
+    }
+}
