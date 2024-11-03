ad2dfac3be9e314ea8dad768f928ea8742342af2
mohmaaya <mohammed.ughratdar@fau.de>
2024-11-03 17:10:31 +0100

    optimizing initialization of app

--- backend/auth/src/main/java/com/bookanaudio/MainApplication.java
+++ backend/auth/src/main/java/com/bookanaudio/MainApplication.java
@@ -3,7 +3,7 @@ package com.bookanaudio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.bookanaudio")
 public class MainApplication {
 
 	public static void main(String[] args) {
--- backend/auth/src/main/java/com/bookanaudio/auth/config/SecurityConfig.java
+++ backend/auth/src/main/java/com/bookanaudio/auth/config/SecurityConfig.java
@@ -2,24 +2,29 @@ package com.bookanaudio.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 @EnableWebSecurity
+@Profile("dev")
 public class SecurityConfig {
 
     // Define the password encoder
     @Bean
+    @Lazy
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     // Define the security filter chain
     @Bean
+    @Lazy
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // Allow all requests
         http
