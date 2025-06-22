21db3199505ff1978680966f364361f814bf914d
mohmaaya <mohammed.ughratdar@fau.de>
2025-06-22 20:32:33 +0200

    added OathLogin Component

---
+++ frontend/src/pages/OAuthSuccess.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useToken } from '../customHooks/TokenContext';
+
+const OAuthSuccess = () => {
+  const navigate = useNavigate();
+  const { setToken } = useToken();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get("token");
+    const username = params.get("username");
+
+    if (token) {
+      setToken(token);
+      localStorage.setItem("username", username || "");
+      navigate("/dashboard^");
+    } else {
+      navigate("/login");
+    }
+  }, []);
+
+  return <p>Logging you in via OAuth...</p>;
+};
+
+export default OAuthSuccess;
